bb75eee2bc72ab31a93283d34daa434b78d48f8d
Andy  Alameddine <andyalam@hotmail.com>
2016-10-14 13:41:37 -0700

    detect when rotations occurs on QuizActivity, save cheat state

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -15,6 +15,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String KEY_CHEATED = "index_cheated";
     private static final int REQUEST_CODE_CHEAT = 0;
 
     private Button mTrueButton;
@@ -127,6 +128,7 @@ public class QuizActivity extends AppCompatActivity {
 
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(KEY_CHEATED, false);
         }
 
         updateQuestion();
@@ -151,6 +153,7 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+        savedInstanceState.putBoolean(KEY_CHEATED, mIsCheater);
     }
 
     @Override
