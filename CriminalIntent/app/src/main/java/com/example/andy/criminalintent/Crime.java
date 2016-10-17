ae9c45246a7b06ad9398f31ad5658f5d08396d64
Andy  Alameddine <andyalam@hotmail.com>
2016-10-17 08:02:12 -0700

    added fragment to main activity

---
+++ CriminalIntent/app/src/main/java/com/example/andy/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package com.example.andy.criminalintent;
+
+
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
