10245cea14262c527fa753575cfdc3419fd412c3
Andy  Alameddine <andyalam@hotmail.com>
2016-10-28 11:42:24 -0700

    base setup for sqlite db

--- CriminalIntent/app/src/main/java/com/example/andy/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/example/andy/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.example.andy.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.andy.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +12,8 @@ import java.util.UUID;
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -18,14 +23,10 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
+
         mCrimes = new ArrayList<>();
-        // populate with generic test crimes
-        /*for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimes.add(crime);
-        }*/
     }
 
     public void addCrime(Crime c) {
