b84931c48173445d8bd1361841d66c804e7e4a19
andy <andyalam@hotmail.com>
2017-03-15 22:15:32 -0700

    geoquizfcc

---
+++ GeoQuizFcc/app/src/main/java/com/example/andy/geoquizfcc/QuizActivity.java
@@ -0,0 +1,127 @@
+package com.example.andy.geoquizfcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private ImageButton mPrevButton;
+    private ImageButton mNextButton;
+    private Button mRandomButton;
+    private TextView mQuestionTextView;
+
+    private int mCurrentIndex = 0;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_ca, false),
+            new Question(R.string.question_sf, false),
+            new Question(R.string.question_la, true),
+            new Question(R.string.question_sd, true)
+    };
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void randomizeQuestionIndex() {
+        int limit = mQuestionBank.length - 1;
+        int randomIndex = new Random().nextInt(limit);
+
+        if (randomIndex == mCurrentIndex) {
+            randomizeQuestionIndex();
+        } else {
+            mCurrentIndex = randomIndex;
+            return;
+        }
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+        //t.setGravity(Gravity.TOP, 0, 0);
+        t.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCurrentIndex == 0) {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                } else {
+                    mCurrentIndex--;
+                }
+                updateQuestion();
+            }
+        });
+
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mRandomButton = (Button) findViewById(R.id.random_button);
+        mRandomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                randomizeQuestionIndex();
+                updateQuestion();
+            }
+        });
+    }
+}
