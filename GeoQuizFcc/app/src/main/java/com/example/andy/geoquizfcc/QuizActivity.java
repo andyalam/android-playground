db1f82cdc38dfa9ef7edeca17511977c0dc34e12
andy <andyalam@hotmail.com>
2017-03-16 23:41:14 -0700

    landscape layout, data persistance via bundle

--- GeoQuizFcc/app/src/main/java/com/example/andy/geoquizfcc/QuizActivity.java
+++ GeoQuizFcc/app/src/main/java/com/example/andy/geoquizfcc/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.andy.geoquizfcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,12 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
+
+    // Tag for debug and other logs
+    private static final String TAG = "QuizActivity";
+
+    // key for savedInstanceBundle
+    private static final String  KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -63,12 +70,51 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() was just called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() was just called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() was just called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() was just called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() was just called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState() called");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate() was just called");
+
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        updateQuestion();
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -123,5 +169,11 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
+        updateQuestion();
     }
 }
