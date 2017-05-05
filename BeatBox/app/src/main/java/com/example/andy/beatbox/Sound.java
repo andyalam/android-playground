ca9f1670ac09a71995b5e9dddf43c777a9f31da0
andyalam <andyalam@hotmail.com>
2017-05-04 23:44:12 -0700

    unit tests, custom sounds, tied up buttons to sounds

--- BeatBox/app/src/main/java/com/example/andy/beatbox/Sound.java
+++ BeatBox/app/src/main/java/com/example/andy/beatbox/Sound.java
@@ -4,6 +4,7 @@ public class Sound {
 
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -26,5 +27,13 @@ public class Sound {
 
     public void setName(String name) {
         this.mName = name;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        this.mSoundId = soundId;
     }
 }
